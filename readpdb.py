050b2f0cfd1ff8530542b05a7465813fc076e5b1
Raghavendran S(CJ) <94078500+cjraghu1998@users.noreply.github.com>
2022-03-15 00:20:15 -0400

    Update readpdb.py

--- readpdb.py
+++ readpdb.py
@@ -4,7 +4,6 @@
 
 from Bio.PDB.PDBParser import PDBParser #import PDBParser from Bio.PDB module 
 from Bio.SeqUtils import seq1 #seq1 part of SeqUtils object converts protein sequence from three-letter to one-letter code.
-import pandas as pd #import pandas
 
 
 #Create PDBParser object
@@ -19,8 +18,8 @@ for chain in model:
     for residue in chain:
         for atom in residue:
             if atom.get_name() == 'CA': 
-                df = print(atom.get_serial_number(), seq1(residue.get_resname()), residue.get_id()[1], chain.get_id())
+                print(atom.get_serial_number(), seq1(residue.get_resname()), residue.get_id()[1], chain.get_id())
 
                 # Prints Atom Number, 1-letter-amino-acid converted Residue Name, Residue number, Chain Name
                 
-df.pd.to_csv('3eml.csv')
+
